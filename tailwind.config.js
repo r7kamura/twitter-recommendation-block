4231ca7b2a4ef6dcda6c2d0fbdd24b09351092df
Ryo Nakamura <r7kamura@gmail.com>
2022-05-30 06:37:56 +0900

    Remove options page and related code

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["options.html", "src/**/*.tsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms")],
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { crx } from "@crxjs/vite-plugin";
-import react from "@vitejs/plugin-react";
 import manifest from "./manifest.json";
 
 export default {
-  plugins: [react(), crx({ manifest })],
+  plugins: [crx({ manifest })],
 };

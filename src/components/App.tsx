bb2d02d4551b33c6a05803abbe6f0ae5f451c416
Ryo Nakamura <r7kamura@gmail.com>
2022-05-29 13:31:52 +0900

    Add prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,8 +4,7 @@ export default function App() {
       <header className="max-w-xl mx-auto p-4 py-8 text-lg">
         <h1 className="font-bold">Twitter Shortcut Plus</h1>
       </header>
-      <main>
-      </main>
+      <main></main>
     </div>
   );
 }

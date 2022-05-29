591d5fc74fb91319e3ae5e2a93b20bc44997f690
Ryo Nakamura <r7kamura@gmail.com>
2022-05-30 06:02:03 +0900

    Remove debug code

--- src/twitter.ts
+++ src/twitter.ts
@@ -14,8 +14,6 @@ export function blockRecommendedTopicsInTimeline() {
 
 export function blockRecommendedUsersInSidebar() {
   findRecommendedUsersSidebarSection().forEach((element) => {
-    console.log(element);
-
     element.style.display = "none";
   });
 }

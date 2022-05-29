bceacb4da599d90e711aa3fdb1a1336cd1824a3c
Ryo Nakamura <r7kamura@gmail.com>
2022-05-29 13:26:19 +0900

    Add options page

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="text-base pb-16">
+      <header className="max-w-xl mx-auto p-4 py-8 text-lg">
+        <h1 className="font-bold">Twitter Shortcut Plus</h1>
+      </header>
+      <main>
+      </main>
+    </div>
+  );
+}
